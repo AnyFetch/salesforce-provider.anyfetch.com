1f7f5b579ce94978f4b6f9d5800ee7e74e3c09cc
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-08 11:34:01 +0100

    Add SF callback

--- config/configuration.js
+++ config/configuration.js
@@ -5,6 +5,17 @@
  * Most of the configuration can be done using system environment variables.
  */
 
+// Instanciate SalesForce URI
+var nforce = require("nforce");
+
+// Create the application instance to manipulate the Salesforce API
+// Optional parameter : It uses the current salesforce API in production mode
+var org = nforce.createConnection({
+  clientId: process.env.SALESFORCE_ID,
+  clientSecret: process.env.SALESFORCE_SECRET,
+  redirectUri: process.env.SALESFORCE_CALLBACK_URL,
+});
+
 // node_env can either be "development" or "production"
 var node_env = process.env.NODE_ENV || "development";
 
@@ -22,9 +33,9 @@ module.exports = {
   port: process.env.PORT || default_port,
   mongo_url: process.env.MONGO_URL || ("mongodb://localhost/provider-salesforce-" + node_env),
 
-  salesforce_id: process.env.SALESFORCE_ID,
-  salesforce_secret: process.env.SALESFORCE_SECRET,
+  salesforce_org: org,
   salesforce_callback: process.env.SALESFORCE_CALLBACK_URL,
+  salesforce_connect: org.getAuthUri(),
 
   cluestr_id: process.env.SALESFORCE_CLUESTR_ID,
   cluestr_secret: process.env.SALESFORCE_CLUESTR_SECRET,
