afa3e0685f9bca458849030a39120251f5f23eb8
Neamar <neamar@neamar.fr>
2014-01-27 15:43:23 +0100

    End of renaming.

--- app.js
+++ app.js
@@ -4,39 +4,7 @@
 var anyfetchProvider = require('anyfetch-provider');
 var serverConfig = require('./lib/provider-salesforce');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/anyfetch-provider');
-var Token = mongoose.model('Token', { anyfetchToken: String, datas: {} });
-
-
-if (serverConfig.env == 'production') {
-  require('newrelic');
-}
-
 var server = anyfetchProvider.createServer(serverConfig);
-
-server.get('/token', function(req, res, next) {
-  var sfToken = req.params.sfid;
-  var anyFetchToken = '';
-
-  Token.find({}, function(err, results) {
-
-    results.forEach(function(storedTokens) {
-      var id = storedTokens.datas.id.slice(storedTokens.datas.id.lastIndexOf('/') + 1);
-      console.log(id);
-      if (sfToken == id) {
-        anyFetchToken = storedTokens.anyfetchToken;
-      }
-    });
-
-    if (anyFetchToken) {
-      res.send(anyFetchToken);
-    } else {
-      res.send(404);
-    }
-
-  });
-});
 
 // Expose the server
 module.exports = server;
