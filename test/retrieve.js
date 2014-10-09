5177f2e311a774c7730130a6afdf04ef1bf406e6
buathi_q <buathi_q@epitech.eu>
2014-10-09 12:50:51 +0200

    Add tests

--- test/retrieve.js
+++ test/retrieve.js
@@ -1,6 +1,46 @@
 'use strict';
 
-require('should');
-var config = require('../config/configuration.js');
+var should = require('should');
+var jsforce = require('jsforce');
 
-// Check data are retrieved from PROVIDER
+var config = require('../config/configuration.js');
+var retrieve = require('../lib/helpers/retrieve.js');
+
+describe("Retrieve code", function () {
+  var conn = new jsforce.Connection({
+    oauth2 : {
+      clientId : config.salesforceId,
+      clientSecret : config.salesforceSecret,
+      redirectUri : config.providerUrl + "/init/callback"
+    },
+    refreshToken: config.testRefreshToken,
+    instanceUrl : 'https://emea.salesforce.com',
+  });
+
+  it("should list contacts", function (done) {
+    retrieve(conn, new Date(0), 50, function(err, newCursor, contacts) {
+      if(err) {
+        return done(err);
+      }
+
+      should.exist(contacts[0]);
+      contacts[0].should.have.property('Id');
+      contacts[0].should.have.property('CreatedDate');
+      contacts[0].should.have.property('LastModifiedDate');
+
+      done();
+    });
+  });
+
+  it("should list contacts modified after specified date", function (done) {
+    retrieve(conn, new Date(2020, 7, 22), 50, function(err, newCursor, contacts) {
+      if(err) {
+        return done(err);
+      }
+
+      contacts.should.have.lengthOf(0);
+
+      done();
+    });
+  });
+});
--- test/upload.js
+++ test/upload.js
@@ -1,7 +1,78 @@
 'use strict';
 
-require('should');
 var request = require('supertest');
 var AnyFetchProvider = require('anyfetch-provider');
+var Anyfetch = require('anyfetch');
+var sinon = require('sinon');
+require('should');
 
-// Check data are uploaded onto AnyFetch
+var config = require('../config/configuration.js');
+var serverConfig = require('../lib/');
+
+describe("Workflow", function () {
+  before(AnyFetchProvider.debug.cleanTokens);
+
+  // Create a fake HTTP server
+  Anyfetch.setApiUrl('http://localhost:1337');
+  var apiServer = Anyfetch.createMockServer();
+  apiServer.listen(1337);
+
+  before(function(done) {
+    AnyFetchProvider.debug.createToken({
+      anyfetchToken: 'fake_gc_access_token',
+      data: {
+        refreshToken: config.testRefreshToken,
+        instanceUrl : 'https://emea.salesforce.com'
+      },
+      cursor: new Date(1970),
+      accountName: 'accountName'
+    }, done);
+  });
+
+  it("should upload data to AnyFetch", function(done) {
+    var count = 0;
+    var originalQueueWorker = serverConfig.workers.addition;
+
+    serverConfig.workers.addition = function(job, cb) {
+      var stub = sinon.stub(job.anyfetchClient, 'postDocument', function(document, cb) { 
+        document.should.have.property('identifier');
+        document.should.have.property('creation_date');
+        document.should.have.property('modification_date');
+
+        cb();
+      });
+
+      originalQueueWorker(job, function(err) {
+        if(err) {
+          throw err;
+        }
+
+        cb(err);
+      });
+
+      stub.called.should.be.true;
+      stub.restore();
+
+      count += 1;
+      if(count === 20) {
+        done();
+      }
+    };
+
+    var server = AnyFetchProvider.createServer(serverConfig.connectFunctions, serverConfig.updateAccount, serverConfig.workers, serverConfig.config);
+
+    request(server)
+      .post('/update')
+      .send({
+        access_token: 'fake_gc_access_token',
+        api_url: 'http://localhost:1337',
+        documents_per_update: 2500
+      })
+      .expect(202)
+      .end(function(err) {
+        if(err) {
+          throw err;
+        }
+      });
+  });
+});
